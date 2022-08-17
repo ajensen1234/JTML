f8a07e49b555147a178e74ccb236319a81c553b3
Andrew <andrewjensen321@gmail.com>
2022-08-17 12:57:22 -0400

    adding ambiguous pose code into discrete functions

--- include/core/sym_trap_functions.h
+++ include/core/sym_trap_functions.h
@@ -1,22 +1,22 @@
 #pragma once
 
 #include "core/data_structures_6D.h"
-#include<cmath>
+#include <cmath>
 #include <vector>
 #include <sstream>
 
 Point6D compute_mirror_pose(Point6D point);
-static void matmult4(float ans[4][4], float matrix1[4][4], float matrix2[4][4]);
-static void matmult3(float ans[3][3], const float matrix1[3][3], const float matrix2[3][3]);
-static void invert_transform(float result[4][4], const float tran[4][4]);
-static void equivalent_axis_angle_rotation(float rot[3][3], const float m[3], const float angle);
-static void cross_product(float CP[3], const float v1[3], const float v2[3]);
-static void dot_product(float& result, const float vector1[3], const float vector2[3]);
-static void rotation_matrix(float R[3][3], Point6D pose);
-static void create_312_transform(float transform[4][4], Point6D pose);
-static void getRotations312(float& xr, float& yr, float& zr, const float Rot[3][3]);
+void matmult4(float ans[4][4], float matrix1[4][4], float matrix2[4][4]);
+void matmult3(float ans[3][3], const float matrix1[3][3], const float matrix2[3][3]);
+void invert_transform(float result[4][4], const float tran[4][4]);
+void equivalent_axis_angle_rotation(float rot[3][3], const float m[3], const float angle);
+void cross_product(float CP[3], const float v1[3], const float v2[3]);
+void dot_product(float& result, const float vector1[3], const float vector2[3]);
+void rotation_matrix(float R[3][3], Point6D pose);
+void create_312_transform(float transform[4][4], Point6D pose);
+void getRotations312(float& xr, float& yr, float& zr, const float Rot[3][3]);
 
-static void copy_matrix_by_value(float(&new_matrix)[3][3], const float(&old_matrix)[3][3]);
+void copy_matrix_by_value(float(&new_matrix)[3][3], const float(&old_matrix)[3][3]);
 void create_vector_of_poses(std::vector<Point6D>& pose_list, Point6D pose, int numPoses);
 
 template<typename T>
---
+++ src/core/ambiguous_pose_processing.cpp
@@ -0,0 +1,25 @@
+#include "core/ambiguous_pose_processing.h"
+
+
+
+Point6D tibial_pose_selector(Point6D femur_pose, Point6D tibia_pose){
+    Point6D tibial_dual_pose = compute_mirror_pose(tibia_pose); // calculate the mirror pose for the tibia
+
+    float vv_original = varus_valgus_calculation(femur_pose, tibia_pose);
+    float vv_mirror   = varus_valgus_calculation(femur_pose, tibia_pose);
+    // if normal pose has a greater VV, return the dual pose.
+    if (vv_original > vv_mirror){
+        return tibial_dual_pose;
+    } 
+    // if normal pose has lower VV than mirror pose, return the normal pose.
+    return tibia_pose;
+}
+
+float varus_valgus_calculation(Point6D& femur_pose, Point6D& tibia_pose){
+    float T_x_fem[4][4], T_x_tib[4][4], T_fem_x[4][4], T_fem_tib[4][4]; // declaring transformation matrices
+    create_312_transform(T_x_fem, femur_pose); // defining transformation matrices
+    create_312_transform(T_x_tib, tibia_pose); 
+    invert_transform(T_fem_x, T_x_fem); // inverting the femoral transformation
+    matmult4(T_fem_tib, T_fem_x, T_x_tib); //multiplying transformation matrices to get tibial coordinates in the femoral reference frame
+    return abs(asin(T_fem_tib[2][1])); // returning the absolute value of the Varus/valgus rotation
+}
