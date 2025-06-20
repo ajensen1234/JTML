ced2c805442f951d05401b9e344b62787b0b5e70
Andrew Jensen <contact@ajensen.org>
2025-06-20 15:47:06 -0400

    fix: removed docs

--- docs/html/search/classes_7.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2023 Gary J. Miller Orthopaedic Biomechanics Lab
- * SPDX-License-Identifier: AGPL-3.0
- */
-
-var searchData=
-[
-  ['locationstorage_0',['LocationStorage',['../class_location_storage.html',1,'']]]
-];
